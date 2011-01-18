b1e93b5b14ce1e2b9d747a490546226330c24df7
joseph <joe@cs.brown.edu>
2011-01-18 09:51:35 -0500

    Use symbols

--- tests/blob/make-timer.rkt
+++ tests/blob/make-timer.rkt
@@ -5,12 +5,12 @@
 
 (define foo
   (blob 0
-        (main (lambda (w the-world)
-                (let ([timer (make-timer 100)])
-                  (handler-complete w '() `(,(register timer "on-tick"))))))
-        (on-tick (lambda (w t)
-                   (display (format "Tick: ~a\n" (add1 w)))
-                   (handler-complete (add1 w))))))
+        ('main (lambda (w the-world)
+                 (let ([timer (make-timer 100)])
+                   (handler-complete w '() `(,(register timer "on-tick"))))))
+        ('on-tick (lambda (w t)
+                    (display (format "Tick: ~a\n" (add1 w)))
+                    (handler-complete (add1 w))))))
 
 (primitive-send foo 'main (MAKE-THE-WORLD))
 
